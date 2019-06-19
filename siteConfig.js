05a6db1daf560af0147815d6d9f7cd71de5295ac
Charles Hall <charles@h-allmedia.com>
2019-06-19 14:52:13 -0400

    new

---
+++ siteConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "Access Today",
+  shortName: "Access",
+  description:
+    "Access is an outcome with zero human barriers.",
+  url: "https://access.today", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Charles Hall",
+  linkedin: "hallcharles",
+  github: "hallmedia",
+  twitter: "hallmedia",
+}
