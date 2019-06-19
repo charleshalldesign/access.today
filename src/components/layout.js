6cf4db6cdac424c44fc25af453f6d48b0829d2fe
Charles Hall <charles@h-allmedia.com>
2019-06-19 14:35:08 -0400

    named footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,9 +63,9 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Charles Hall. Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.gatsbyjs.org">Gatsby</a>. Conforms to WCAG 2.1 AA.
         </footer>
       </div>
     )
